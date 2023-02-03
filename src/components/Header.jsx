c15ba2d725442cb4e63670c149c50f9e50d0dabc
Nguyen Quang Tri <nguyenquangtri@Nguyens-Mac-mini.local>
2023-02-03 14:46:48 +0700

    fix logo hidden

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,7 +51,7 @@ export default function Header() {
         >
           <div className="flex w-full items-center justify-between border-b border-indigo-500 py-6 lg:border-none">
             <div className="flex items-center">
-              <div className="hidden space-x-8 lg:block">
+              <div className="hidden space-x-8 lg:!block">
                 {navigation.map((link) => (
                   <a
                     key={link.name}
